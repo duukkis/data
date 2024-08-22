c9d2ad41355bc3d9a7c88a42712fa93e122b3979
duukkis <duukkis@hotmail.com>
2024-08-22 14:48:01 +0300

    overwrite existing

--- scripts/parseheadlines/cleanup.php
+++ scripts/parseheadlines/cleanup.php
@@ -3,14 +3,25 @@
  * file to clean up bad voikko from uutisotsikot
  */
 
-
 $paikannimet = explode(PHP_EOL, file_get_contents("../../uutisotsikot/paikannimi.txt"));
+$etuliitteet = ["NHL", "äiti", "EM", "MM", "EU", "Missi", "TV", "äiti"];
 
-if (false) {
-    $c = file_get_contents("../../uutisotsikot/etunimi.txt");
+$debug = true;
+$runOnFiles = [
+    "etunimi" => false,
+    "laatusana" => false,
+    "lukusana" => false,
+    "nimisana" => false,
+    "sukunimi" => true,
+    "teonsana" => false,
+];
+
+if ($runOnFiles["etunimi"]) {
+    $filename = "../../uutisotsikot/etunimi.txt";
+    $c = file_get_contents($filename);
     $p = explode(PHP_EOL, $c);
 
-    $badFirsts = array_merge($paikannimet, ["NHL", "äiti", "EM", "MM", "EU", "Missi", "TV"]);
+    $badFirsts = array_merge($paikannimet, $etuliitteet);
     $badSeconds = ["Päivä", "Sisko", "Akseli"];
 
     unset($badFirsts[array_search("Kaija", $badFirsts)]);
@@ -23,51 +34,136 @@ if (false) {
         $pcs = explode("-", $val);
         if (count($pcs) >= 2) {
             if (in_array($pcs[0], $badFirsts, true) || in_array($pcs[1], $badSeconds, true)) {
-                print $val . PHP_EOL;
+                if ($debug) {
+                    print $val . PHP_EOL;
+                } else {
+                    unset($p[$i]);
+                }
             }
         }
     }
+    if (!$debug) {
+        file_put_contents($filename, implode(PHP_EOL, $p));
+    }
 }
 
-if (false) {
-    $c = file_get_contents("../../uutisotsikot/laatusana.txt");
+if ($runOnFiles["nimisana"]) {
+    $filename = "../../uutisotsikot/nimisana.txt";
+    $c = file_get_contents($filename);
     $p = explode(PHP_EOL, $c);
     foreach ($p as $i => $val) {
         // first bad - stuff
         $pcs = explode("-", $val);
         if (count($pcs) >= 2) {
             if (in_array($pcs[0], $paikannimet, true)) {
-                print $val . PHP_EOL;
+                if ($debug) {
+                    print $val . PHP_EOL;
+                } else {
+                    unset($p[$i]);
+                }
             }
         }
     }
+    if (!$debug) {
+        file_put_contents($filename, implode(PHP_EOL, $p));
+    }
 }
 
-if (false) {
+if ($runOnFiles["laatusana"]) {
+    $filename = "../../uutisotsikot/laatusana.txt";
+    $c = file_get_contents($filename);
+    $p = explode(PHP_EOL, $c);
+    foreach ($p as $i => $val) {
+        // first bad - stuff
+        $pcs = explode("-", $val);
+        if (count($pcs) >= 2) {
+            if (in_array($pcs[0], $paikannimet, true)) {
+                if ($debug) {
+                    print $val . PHP_EOL;
+                } else {
+                    unset($p[$i]);
+                }
+            }
+        }
+    }
+    if (!$debug) {
+        file_put_contents($filename, implode(PHP_EOL, $p));
+    }
+}
+
+if ($runOnFiles["lukusana"]) {
     $romanNumerals = ["ii", "iii", "iv", "ix", "vi", "vii", "viii"];
-    $c = file_get_contents("../../uutisotsikot/lukusana.txt");
+    $filename = "../../uutisotsikot/lukusana.txt";
+    $c = file_get_contents($filename);
     $p = explode(PHP_EOL, $c);
     foreach ($p as $i => $val) {
         $camp = mb_strtolower($val);
         if (in_array(mb_substr($camp, 0, 1), ["-", "x"], true) || in_array($camp, $romanNumerals, true)) {
-            print $val . PHP_EOL;
+            if ($debug) {
+                print $val . PHP_EOL;
+            } else {
+                unset($p[$i]);
+            }
         }
+    }
+    if (!$debug) {
+        file_put_contents($filename, implode(PHP_EOL, $p));
     }
 }
 
-if (false) {
-    $badFirsts = array_merge($paikannimet, ["NHL", "äiti", "EM", "MM", "EU", "Missi", "TV"]);
+if ($runOnFiles["sukunimi"]) {
+    $badFirsts = array_merge($paikannimet, $etuliitteet);
+    unset($badFirsts[array_search("Väinölä", $badFirsts)]);
+    unset($badFirsts[array_search("Ukkola", $badFirsts)]);
+    unset($badFirsts[array_search("Takalo", $badFirsts)]);
+    unset($badFirsts[array_search("Tervo", $badFirsts)]);
+    unset($badFirsts[array_search("Tervola", $badFirsts)]);
+    unset($badFirsts[array_search("Salmela", $badFirsts)]);
+    unset($badFirsts[array_search("Pisa", $badFirsts)]);
+    unset($badFirsts[array_search("Mikkola", $badFirsts)]);
+    unset($badFirsts[array_search("Koski", $badFirsts)]);
 
-    $c = file_get_contents("../../uutisotsikot/teonsana.txt");
+    $filename = "../../uutisotsikot/sukunimi.txt";
+    $c = file_get_contents($filename);
     $p = explode(PHP_EOL, $c);
     foreach ($p as $i => $val) {
         // first bad - stuff
         $pcs = explode("-", $val);
         if (count($pcs) >= 2) {
             if (in_array($pcs[0], $badFirsts, true)) {
-                print $val . PHP_EOL;
+                if ($debug) {
+                    print $val . PHP_EOL;
+                } else {
+                    unset($p[$i]);
+                }
             }
         }
+    }
+    if (!$debug) {
+        file_put_contents($filename, implode(PHP_EOL, $p));
+    }
+}
+
+if ($runOnFiles["teonsana"]) {
+    $badFirsts = array_merge($paikannimet, $etuliitteet);
+    $filename = "../../uutisotsikot/teonsana.txt";
+    $c = file_get_contents($filename);
+    $p = explode(PHP_EOL, $c);
+    foreach ($p as $i => $val) {
+        // first bad - stuff
+        $pcs = explode("-", $val);
+        if (count($pcs) >= 2) {
+            if (in_array($pcs[0], $badFirsts, true)) {
+                if ($debug) {
+                    print $val . PHP_EOL;
+                } else {
+                    unset($p[$i]);
+                }
+            }
+        }
+    }
+    if (!$debug) {
+        file_put_contents($filename, implode(PHP_EOL, $p));
     }
 }
 
